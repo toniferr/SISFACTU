a7e609e02572b6d32b5387a8ff62225250268808
Toni <ferreirocouto@gmail.com>
2019-02-15 20:23:46 +0100

    Subida inicial del proyecto

---
+++ src/main/java/com/toni/ferreiro/SisfactuApplication.java
@@ -0,0 +1,14 @@
+package com.toni.ferreiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisfactuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisfactuApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/toni/ferreiro/SisfactuApplicationTests.java
@@ -0,0 +1,17 @@
+package com.toni.ferreiro;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SisfactuApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
