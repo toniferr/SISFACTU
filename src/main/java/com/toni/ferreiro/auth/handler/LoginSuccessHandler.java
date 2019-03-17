7fdcd2377cafc8c784d64dfb4e3f685dcd575026
toni <ferreirocouto@gmail.com>
2019-03-17 18:45:57 +0100

    usuario autenticado en controller

--- src/main/java/com/toni/ferreiro/auth/handler/LoginSuccessHandler.java
+++ src/main/java/com/toni/ferreiro/auth/handler/LoginSuccessHandler.java
@@ -21,8 +21,12 @@ public class LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 		
 		SessionFlashMapManager flashMapManager = new SessionFlashMapManager();
 		FlashMap flashMap = new FlashMap();
-		flashMap.put("success", "Ha iniciado sesión con éxito");
+		flashMap.put("success", "Hola "+authentication.getName()+", has iniciado sesión con éxito");
 		flashMapManager.saveOutputFlashMap(flashMap, request, response);
+		if(authentication != null) {
+			logger.info("El usuario '"+authentication.getName()+"' ha iniciado sesión con éxito");
+		}
+		
 		super.onAuthenticationSuccess(request, response, authentication);
 	}
 
--- src/main/java/com/toni/ferreiro/controllers/ClienteController.java
+++ src/main/java/com/toni/ferreiro/controllers/ClienteController.java
@@ -1,10 +1,13 @@
 package com.toni.ferreiro.controllers;
+
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.util.Map;
 
 import javax.validation.Valid;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.data.domain.Page;
@@ -12,6 +15,8 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -34,6 +39,8 @@ import com.toni.ferreiro.util.paginator.PageRender;
 @SessionAttributes("cliente")
 public class ClienteController {
 
+	private final Log logger = LogFactory.getLog(this.getClass());
+	
 	@Autowired
 	private ClienteServiceInterface clienteService;
 
@@ -68,8 +75,19 @@ public class ClienteController {
 		return "ver";
 	}
 
-	@RequestMapping(value = {"/listar","/"}, method = RequestMethod.GET)
-	public String listar(@RequestParam(name = "page", defaultValue = "0") int page, Model model) {
+	@RequestMapping(value = { "/listar", "/" }, method = RequestMethod.GET)
+	public String listar(@RequestParam(name = "page", defaultValue = "0") int page, Model model,
+			Authentication authentication) {
+		
+		if (authentication != null){
+			logger.info("Hola usuario autenticado, tu username es :".concat(authentication.getName()));
+		}
+		
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		
+		if (auth != null){
+			logger.info("Utilizando forma estática SecurityContextHolder.getContext().getAuthentication(): usuario autenticado, username :".concat(auth.getName()));
+		}
 
 //		Pageable pageRequest = new PageRequest(page, 5); PageRequest is deprecated con spring boot 2
 		Pageable pageRequest = PageRequest.of(page, 5);
