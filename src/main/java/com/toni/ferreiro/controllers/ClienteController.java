a2736d799e81da3b858907323dd155211b7244ef
toni <ferreirocouto@gmail.com>
2019-03-17 21:39:54 +0100

    implementar metodo has role

--- src/main/java/com/toni/ferreiro/controllers/ClienteController.java
+++ src/main/java/com/toni/ferreiro/controllers/ClienteController.java
@@ -2,6 +2,7 @@ package com.toni.ferreiro.controllers;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.util.Collection;
 import java.util.Map;
 
 import javax.validation.Valid;
@@ -16,6 +17,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -40,7 +44,7 @@ import com.toni.ferreiro.util.paginator.PageRender;
 public class ClienteController {
 
 	private final Log logger = LogFactory.getLog(this.getClass());
-	
+
 	@Autowired
 	private ClienteServiceInterface clienteService;
 
@@ -78,15 +82,23 @@ public class ClienteController {
 	@RequestMapping(value = { "/listar", "/" }, method = RequestMethod.GET)
 	public String listar(@RequestParam(name = "page", defaultValue = "0") int page, Model model,
 			Authentication authentication) {
-		
-		if (authentication != null){
+
+		if (authentication != null) {
 			logger.info("Hola usuario autenticado, tu username es :".concat(authentication.getName()));
 		}
-		
+
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		
-		if (auth != null){
-			logger.info("Utilizando forma estática SecurityContextHolder.getContext().getAuthentication(): usuario autenticado, username :".concat(auth.getName()));
+
+		if (auth != null) {
+			logger.info(
+					"Utilizando forma estática SecurityContextHolder.getContext().getAuthentication(): usuario autenticado, username :"
+							.concat(auth.getName()));
+		}
+
+		if (hasRole("ROLE_ADMIN")) {
+			logger.info("Hola ".concat(auth.getName().concat(" tienes acceso")));
+		} else {
+			logger.info("Hola ".concat(auth.getName().concat(" no tienes acceso")));
 		}
 
 //		Pageable pageRequest = new PageRequest(page, 5); PageRequest is deprecated con spring boot 2
@@ -182,6 +194,26 @@ public class ClienteController {
 		}
 
 		return "redirect:/listar";
+	}
+
+	private boolean hasRole(String role) {
+		SecurityContext context = SecurityContextHolder.getContext();
+		if (context == null) {
+			return false;
+		}
+		Authentication auth = context.getAuthentication();
+		if (auth == null) {
+			return false;
+		}
+		Collection<? extends GrantedAuthority> authorities = auth.getAuthorities();
+		/*for (GrantedAuthority authority : authorities) {
+			if (role.equals(authority.getAuthority())) {
+				logger.info("Hola ".concat(auth.getName().concat(" ,tu rol es ").concat(authority.getAuthority())));
+				return true;
+			}
+		}
+		return false;*/
+		return authorities.contains(new SimpleGrantedAuthority(role));
 	}
 
 }
