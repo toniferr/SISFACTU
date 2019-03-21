ba2019c888ac6caafefb1402e6851486daaaacda
toni <ferreirocouto@gmail.com>
2019-03-21 21:15:48 +0100

    Multilenguaje con ?lang

--- src/main/java/com/toni/ferreiro/MvcConfig.java
+++ src/main/java/com/toni/ferreiro/MvcConfig.java
@@ -1,5 +1,7 @@
 package com.toni.ferreiro;
 
+import java.util.Locale;
+
 import org.springframework.context.annotation.Bean;
 
 //import java.nio.file.Paths;
@@ -8,10 +10,14 @@ import org.springframework.context.annotation.Bean;
 //import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 //import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 //import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer{ //se añade para spring boot 2
@@ -36,6 +42,25 @@ public class MvcConfig implements WebMvcConfigurer{ //se añade para spring boot
 	@Bean //cuando no es propia no es @Component
 	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public LocaleResolver localeResolver(){
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(new Locale("es","ES"));
+		return localeResolver;		
+	}
+	
+	@Bean
+	public LocaleChangeInterceptor localeChangeInterceptor() {
+		LocaleChangeInterceptor localeInterceptor = new LocaleChangeInterceptor();
+		localeInterceptor.setParamName("lang");
+		return localeInterceptor;
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(localeChangeInterceptor());
 	}
 	
 }
