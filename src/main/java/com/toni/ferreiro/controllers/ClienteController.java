b29bec5b30189716a8698383811b33389aef8874
toni <ferreirocouto@gmail.com>
2019-03-22 20:54:13 +0100

    Traducción en el controller

--- src/main/java/com/toni/ferreiro/controllers/ClienteController.java
+++ src/main/java/com/toni/ferreiro/controllers/ClienteController.java
@@ -3,6 +3,7 @@ package com.toni.ferreiro.controllers;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.util.Collection;
+import java.util.Locale;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
@@ -11,6 +12,7 @@ import javax.validation.Valid;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.core.io.Resource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -54,6 +56,9 @@ public class ClienteController {
 
 	@Autowired
 	private UploadFileServiceInterface uploadFileService;
+	
+	@Autowired
+	private MessageSource messageSource;
 
 	@Secured({"ROLE_USER","ROLE_ADMIN"}) //se podria dejar un solo rol entre llaves
 	@GetMapping(value = "/upload/{filename:.+}")
@@ -87,7 +92,7 @@ public class ClienteController {
 
 	@RequestMapping(value = { "/listar", "/" }, method = RequestMethod.GET)
 	public String listar(@RequestParam(name = "page", defaultValue = "0") int page, Model model,
-			Authentication authentication, HttpServletRequest request) {
+			Authentication authentication, HttpServletRequest request, Locale locale) {
 
 		if (authentication != null) {
 			logger.info("Hola usuario autenticado, tu username es :".concat(authentication.getName()));
@@ -127,7 +132,8 @@ public class ClienteController {
 
 		PageRender<Cliente> pageRender = new PageRender<>("/listar", clientes);
 
-		model.addAttribute("titulo", "Listado de clientes");
+//		model.addAttribute("titulo", "Listado de clientes");
+		model.addAttribute("titulo", messageSource.getMessage("text.cliente.listar.titulo",null, locale));
 		model.addAttribute("clientes", clientes);
 		model.addAttribute("page", pageRender);
 		return "listar";
